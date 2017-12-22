fcae5826027023c88100f966aaabdd3b14cf62ba
Jnweb <jnweb@jnweb.com.br>
2017-12-22 13:46:06 -0200

    tela de login

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-
+session_start();
 require_once("vendor/autoload.php");
 
 use Slim\App;
 use Jnwebsi\Page;
 use Jnwebsi\PageAdmin;
+use Jnwebsi\Model\User;
 
 $app = new Slim\App();
 
-$app->get('/', function () {
+// $app->config('debug', true);
+
+$app->get('/', function() {
 
 	$page = new Page();
 
@@ -16,12 +19,39 @@ $app->get('/', function () {
 
 }); // ROTA INDEX
 
-$app->get('/admin', function () {
+$app->get('/admin', function() {
+
+	User::verifyLogin();
 
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
 }); // ROTA ADMIN
+
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("login");
+
+}); // ROTA LOGIN
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+	User::logout();
+	header("Location: /admin/login");
+	exit;
+});
 
 $app->run();
